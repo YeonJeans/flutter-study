4491a7623fb827789924d37345dca8d5120194c7
mori8 <kaithape@gmail.com>
2022-12-11 19:40:43 +0900

    [week3] 화면 이동

--- 남수연/week2/my_app/lib/main.dart
+++ 남수연/week2/my_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'map.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,106 +11,127 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      // EdgeInsets.all() creates a symmetrical padding on all sides of the container
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          // Expanded widget allows the column to take up all the remaining space
-          // flex-1
-          Expanded(
-            child: Column(
-              // crossAxisAlignment: justify-content
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  // EdgeInsets.only() creates a padding on the top and bottom of the container
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(color: Colors.grey[500]),
-                ),
-              ],
-            ),
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
-        ],
-      ),
-    );
+    double densityAmt = -1.0;
+    VisualDensity density =
+        VisualDensity(horizontal: densityAmt, vertical: densityAmt);
 
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+        '/map': (context) => Map(),
+      },
+      theme: ThemeData(visualDensity: density),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget titleSection = Container(
+    // EdgeInsets.all() creates a symmetrical padding on all sides of the container
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        // Expanded widget allows the column to take up all the remaining space
+        Expanded(
+          child: Column(
+            // crossAxisAlignment: justify-content
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                // EdgeInsets.only() creates a padding on the top and bottom of the container
+                padding: const EdgeInsets.only(bottom: 8),
+                child: const Text(
+                  'Oeschinen Lake Campground',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(color: Colors.grey[500]),
+              ),
+            ],
+          ),
+        ),
+        Icon(
+          Icons.star,
+          color: Colors.red[500],
+        ),
+        const Text('41'),
+      ],
+    ),
+  );
+
+  Widget textSection = const Padding(
+    padding: EdgeInsets.all(32),
+    child: Text(
+      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+      'Alps. Situated 1,578 meters above sea level, it is one of the '
+      'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+      'half-hour walk through pastures and pine forest, leads you to the '
+      'lake, which warms to 20 degrees Celsius in the summer. Activities '
+      'enjoyed here include rowing, and riding the summer toboggan run.',
+      softWrap: true,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
     Color color = Theme.of(context).primaryColor;
 
     Widget buttonSection = Row(
       // mainAxisAlignment: align-items
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        _buildButtonColumn(color, Icons.call, 'CALL'),
-        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-        _buildButtonColumn(color, Icons.share, 'SHARE')
+        _buildButtonColumn(color, Icons.call, 'CALL', context, '/map'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE', context, '/map'),
+        _buildButtonColumn(color, Icons.share, 'SHARE', context, '/map')
       ],
     );
 
-    Widget textSection = const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-
-    return MaterialApp(
-        title: 'Flutter layout demo',
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Flutter layout demo'),
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/lake.jpeg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
             ),
-            body: ListView(
-              children: [
-                Image.asset(
-                  'images/lake.jpeg',
-                  width: 600,
-                  height: 240,
-                  fit: BoxFit.cover,
-                ),
-                titleSection,
-                buttonSection,
-                textSection
-              ],
-            )));
+            titleSection,
+            buttonSection,
+            textSection
+          ],
+        ));
   }
 
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
+  GestureDetector _buildButtonColumn(Color color, IconData icon, String label,
+      BuildContext context, String route) {
+    return GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, route);
+          print('tapped $label');
+        },
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, color: color),
+            Container(
+              margin: const EdgeInsets.only(top: 8),
+              child: Text(
+                label,
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: color,
+                ),
+              ),
             ),
-          ),
-        ),
-      ],
-    );
+          ],
+        ));
   }
 }
