b323d537159030b7c93bde44aa036e5f9cac6d97
mori8 <kaithape@gmail.com>
2022-12-13 16:47:38 +0900

    [week3] Animation

--- 남수연/week2/my_app/lib/map.dart
+++ 남수연/week2/my_app/lib/map.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Map extends StatefulWidget {
   @override
@@ -7,15 +8,28 @@ class Map extends StatefulWidget {
 }
 
 class _MapState extends State<Map> {
+  var _size = 100.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Map'),
-      ),
-      body: Center(
-        child: Text('Map'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('AnimatedContainer'),
+        ),
+        body: Center(
+            child: GestureDetector(
+                onTap: () {
+                  final random = Random();
+                  setState(() {
+                    _size = random.nextInt(200).toDouble() + 100;
+                  });
+                },
+                child: AnimatedContainer(
+                  width: _size,
+                  height: _size,
+                  color: Colors.blue,
+                  duration: Duration(seconds: 1),
+                  curve: Curves.fastOutSlowIn,
+                ))));
   }
 }
