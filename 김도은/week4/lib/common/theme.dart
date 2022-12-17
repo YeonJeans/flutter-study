2e5c4b1c1ed506784fd80571e4e2ba274d16b5b8
whaeundo <1916388@sookmyung.ac.kr>
2022-12-17 23:24:20 +0900

    [week4] 상태 관리 실습 진행

---
+++ 김도은/week4/lib/common/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+final appTheme = ThemeData(
+  primarySwatch: Colors.yellow,
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(
+      fontFamily: 'Corben',
+      fontWeight: FontWeight.w700,
+      fontSize: 24,
+      color: Colors.black,
+    ),
+    displayMedium: TextStyle(
+      fontFamily: 'Corben',
+      fontWeight: FontWeight.w300,
+      fontSize: 10,
+      color: Colors.black,
+    ),
+  ),
+);
---
+++ 김도은/week4/lib/screens/login.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatelessWidget {
+  const MyLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(80.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Username',
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/catalog');
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.yellow,
+                ),
+                child: Container(
+                  height: 40,
+                  child:Center(
+                    child: Text(
+                        '확인',
+                        style: TextStyle(fontSize: 14, color: Colors.black)
+                    )
+                  )
+                )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
